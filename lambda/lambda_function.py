884c3e7c646ac2f685ee28b028d0a300e0c960af
Dylan Corr <dylancorr.g@gmail.com>
2024-11-24 12:33:34 +0000

    initial set up for git hub actions

---
+++ lambda/lambda_function.py
@@ -0,0 +1,17 @@
+
+def lambda_handler(event, context):
+    """
+    This function is a basic AWS Lambda handler that returns a simple JSON response.
+
+    Args:
+        event (dict): The event dictionary passed to the Lambda function.
+        context (object): The context object passed to the Lambda function.
+
+    Returns:
+        dict: A dictionary containing the HTTP status code (200) and a JSON body with the message "Hello from CIDC Lambda".
+    """
+    return {
+        'statusCode': 200,  # Indicate successful execution
+        'body': json.dumps("Hello from CIDC Lambda")  # Serialize the message as JSON
+    }
+
